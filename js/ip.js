b668884e61a123f557a3529cea262b427774a64c
Alexuber <alexuberbuber@gmail.com>
2023-05-05 18:07:10 +0300

    add post req

--- js/ip.js
+++ js/ip.js
@@ -7,8 +7,22 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  console.log(
-    " -->",
-    position.coords.latitude + "/" + position.coords.longitude
-  );
+  const pos = {
+    lat: position.coords.latitude,
+    long: position.coords.longitude,
+  };
+
+  fetch("http://localhost:3000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pos),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("content loaded");
+    });
 }
